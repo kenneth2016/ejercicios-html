169c4bc0704778b88e35a176eecdd9babd809329
joshuaavalos74 <joshuaavalos74@gmail.com>
2017-03-14 14:08:03 -0600

    dfyiyt

---
+++ elefante3.py
@@ -0,0 +1,6 @@
+cantidad = int(Input("cuantas veces quiere cantar"))
+a=1
+for a in range (cantida)
+		print("un elefante se columpiaba sobre la tela de una arañay como vieron que resistia fueron a llamar a otro elefante")
+	
+	print ("gracias por cantar conmigo")
